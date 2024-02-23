029ef970ffd9bbb644ac5ffb33ec0c13865804b4
Kamila <kamila.mspychala@vp.pl>
2024-02-23 16:11:39 +0100

    Entry Ilustrations

---
+++ Assets/Scripts/Crystal Lake/Entry Ilustrations/EntryIllustrationsController.cs
@@ -0,0 +1,17 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryIllustrationsController : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+
+
+    }
+
+}
